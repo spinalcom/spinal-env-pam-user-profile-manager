e87a72ba2c230462f920c7251f4da006a1083acc
spinalcom <mbaradji221@outlook.fr>
2022-11-03 15:01:24 +0100

    add token interceptor

--- src/requests/index.ts
+++ src/requests/index.ts
@@ -29,7 +29,11 @@ const host = (process.env.SPINAL_API_URL || "").replace(`/\/$/`, el => "");
 const baseURL = host.match(new RegExp(endpoint)) ? host : host + endpoint;
 
 const http = axios.create({ baseURL });
-
+http.interceptors.request.use((request: any) => {
+    const t = localStorage.getItem('token');
+    if (t) request.headers.common.Authorization = `Bearer ${t}`;
+    return request;
+});
 
 export function getAllUserProfilesRequest() {
     try {
